bf08535015f4345c6f44a91e4086eb75e54bc028
maaari330 <186826733+maaari330@users.noreply.github.com>
2025-12-10 22:05:27 +0900

    adapt css

--- stock-frontend/app/page.tsx
+++ stock-frontend/app/page.tsx
@@ -4,46 +4,46 @@ import { BsGraphUpArrow } from "react-icons/bs";
 import { BsGraphDownArrow } from "react-icons/bs";
 
 export default function Home() {
-  const [Ticker,setTicker]=useState("")
-  const [Days,setDays]=useState("")
-  const [PredictionLabel,setPredictionLabel]=useState("")
-  const [Confidence,setConfidence]=useState(0)
+  const [Ticker, setTicker] = useState("")
+  const [Days, setDays] = useState("")
+  const [PredictionLabel, setPredictionLabel] = useState("")
+  const [Confidence, setConfidence] = useState(0)
 
-  async function handleSubmit(e: React.FormEvent<HTMLFormElement>){
+  async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
-    const res=await fetch('http://localhost:8000/predict',{method:'POST',headers: {'Content-Type': 'application/json'},body:JSON.stringify({Ticker:Ticker,Days:Number(Days)})})
-    const resBody= await res.json()
+    const res = await fetch('http://localhost:8000/predict', { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify({ Ticker: Ticker, Days: Number(Days) }) })
+    const resBody = await res.json()
     setPredictionLabel(resBody.PredictionLabel)
     setConfidence(resBody.Confidence)
   }
 
   return (
-    <>
+    <main className="page">
       <div>
         株価予測アプリ
       </div>
-      <form method="post" onSubmit={handleSubmit}>
+      <form method="post" onSubmit={handleSubmit} className="forms">
         <label>
           銘柄コード/Ticker:
-          <input type="text" name="ticker" value={Ticker} onChange={e => setTicker(e.target.value)}/>
+          <input type="text" name="ticker" value={Ticker} onChange={e => setTicker(e.target.value)} />
         </label>
         <label>
-          日数: 
-          <input type="text" name="days" value={Days} onChange={e => setDays(e.target.value)}/>
+          日数:
+          <input type="text" name="days" value={Days} onChange={e => setDays(e.target.value)} />
         </label>
         <button type="submit">株価予測</button>
       </form>
-      {PredictionLabel && 
+      {PredictionLabel &&
         <div className="w-md px-4 ml-2 mr-10 border-double border-sky-200">
-          {PredictionLabel=="UP"?(
-            <p className={"font-semibold text-green-300"}>明日の株価予測：{PredictionLabel}<BsGraphUpArrow/></p>
-          ):(
-            <p className={"font-semibold text-red-300"}>明日の株価予測：{PredictionLabel}<BsGraphDownArrow/></p>
+          {PredictionLabel == "UP" ? (
+            <p className={"font-semibold text-green-300"}>明日の株価予測：{PredictionLabel}<BsGraphUpArrow /></p>
+          ) : (
+            <p className={"font-semibold text-red-300"}>明日の株価予測：{PredictionLabel}<BsGraphDownArrow /></p>
           )}
-          <p>予測精度：{Math.round(Confidence*100)}%</p>
-          <progress value={Confidence*100} max={100} />
+          <p>予測精度：{Math.round(Confidence * 100)}%</p>
+          <progress value={Confidence * 100} max={100} />
         </div>
       }
-    </>
+    </main>
   );
 }
